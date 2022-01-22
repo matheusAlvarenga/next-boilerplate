75b35fe64fef0cd8dcb3ab9e511e9f4e256609bd
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-01-22 16:03:07 -0300

    chore: add typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        Welcome to Next.js Boilerplate
+      </main>
+    </div>
+  )
+}
