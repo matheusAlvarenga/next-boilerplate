569b91c42db55a46bb6d7601dd322979c0b7287f
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-01-22 16:27:12 -0300

    chore: add styled components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { AppProps } from "next/app";
+import { ResetStyle } from "../styles";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <ResetStyle />
+  </>
 );
 
 export default MyApp;
