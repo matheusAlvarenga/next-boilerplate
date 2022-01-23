b4d96b2545787f354eda2fcf78ef14c9be29c11f
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-01-22 21:26:15 -0300

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-const withImages = require('next-images')
+const withPlugins = require('next-compose-plugins')
 const { i18n } = require('./next-i18next.config')
-
-module.exports = withImages({
+const withImages = require('next-images')({
   esModule: true,
   i18n
 })
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withPlugins([
+  [withBundleAnalyzer, withImages],
+])
