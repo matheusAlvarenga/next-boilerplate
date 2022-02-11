2c06ea1555d9f9164fde8b7e1a9750a76f3f5397
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-02-11 20:55:03 -0300

    fix(storybook): :rotating_light: fix eslint errors

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Button, ButtonProps } from './button';
+import { Button, ButtonProps } from "./button";
 
 export default {
-  title: 'Example/Header',
+  title: "Example/Header",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-export const Template: ComponentStory<typeof Button> = (args:ButtonProps) => <Button {...args} >Test</Button>;
-
+export const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
+  <Button {...args}>Test</Button>
+);
