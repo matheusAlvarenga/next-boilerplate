491c6b4f025aff2658a2a020468b7b244597731b
matheusAlvarenga <matheus.alvarenga@natahouse.com>
2022-02-12 10:50:27 -0300

    refactor(storybook): :recycle: update storybook structure on storie template

--- src/components/button.stories.tsx
+++ src/components/button.stories.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Story, Meta } from "@storybook/react/types-6-0";
 
 import { Button, ButtonProps } from "./button";
 
 export default {
   title: "Example/Header",
   component: Button,
-} as ComponentMeta<typeof Button>;
+} as Meta;
 
-export const Template: ComponentStory<typeof Button> = (args: ButtonProps) => (
-  <Button {...args}>Test</Button>
+const Template: Story<ButtonProps & { label: string }> = args => (
+  // eslint-disable-next-line react/destructuring-assignment
+  <Button {...args}>{args.label}</Button>
 );
+
+export const Default = Template.bind({});
+Default.args = {
+  styling: "main",
+  label: "Example Button",
+};
